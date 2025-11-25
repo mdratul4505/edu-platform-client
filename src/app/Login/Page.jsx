64ab5e78577795c642ed71612618cacd0dc046ef
mdratul4505 <dev.raul.coder@gmail.com>
2025-11-26 00:10:26 +0600

    added about section

---
+++ src/app/Login/Page.jsx
@@ -0,0 +1,77 @@
+'use client'
+import { useState } from 'react';
+import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth'
+
+import { useRouter } from 'next/navigation';
+import { signInWithPopup } from "firebase/auth"
+import { auth, provider } from '../Firebase/config';
+
+
+
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const router = useRouter()
+
+  const handleSignIn = async () => {
+    try {
+        const res = await signInWithEmailAndPassword(email, password);
+        console.log({res});
+        sessionStorage.setItem('user', true)
+        setEmail('');
+        setPassword('');
+        router.push('/')
+    }catch(e){
+        console.error(e)
+    }
+  };
+   const handleGoogleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+      sessionStorage.setItem('user', true)
+      router.push('/')
+    } catch (error) {
+      console.error(error);
+    }
+   }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
+        <h1 className="text-white text-2xl mb-5">Sign In</h1>
+        <input 
+          type="email" 
+          placeholder="Email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <button 
+          onClick={handleSignIn}
+          className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
+        >
+          Sign In
+        </button>
+
+         <button 
+          onClick={handleGoogleSignIn}
+          className="w-full p-3 mt-5 bg-indigo-600 rounded text-white hover:bg-indigo-500"
+        >
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
